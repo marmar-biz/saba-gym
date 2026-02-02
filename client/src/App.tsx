4b2ad601b19d49dde2698feabd3f16216e504052
marmar-biz <biazar.insurance@gmail.com>
2026-02-02 15:47:48 +0330

    Add main App component with routing and context

---
+++ client/src/App.tsx
@@ -0,0 +1,35 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { Layout } from "@/components/Layout";
+import Home from "@/pages/Home";
+import Services from "@/pages/Services";
+import Request from "@/pages/Request";
+import Contact from "@/pages/Contact";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/request" component={Request} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <Router />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
